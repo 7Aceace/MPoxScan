f073de5c062f5e6b4d5f31f339f7c76737b0f2ec
Ace Monte <ace.monte27@gmail.com>
2024-11-07 22:07:30 +0800

    Tailwindcss,EsLint and Prettier--unfinished dependencies

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname, { isCSSEnabled: true })
+const config = getDefaultConfig(path.resolve(__dirname), { isCSSEnabled: true });
 
-module.exports = withNativeWind(config, { input: './global.css' })
+
+module.exports = withNativeWind(config, { input: './global.css' });
