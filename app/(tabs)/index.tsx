3ab6bcea0c76627cdf4ce9574c242662e25ddf8a
Gel <markepanganoron@su.edu.ph>
2024-11-09 17:29:16 +0800

    ResultPrototype1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { StatusBar, Text } from 'react-native';
+import { StatusBar, Text, View} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-green-400">
+      <View className='flex-1 flex-row align-left'><Text className='text-white content-start text-left'>Back</Text><Text className='text-white'>Results</Text></View>
       <Text className="text-2xl font-bold text-white">MPoxScan Gelson push trial</Text>
       <StatusBar style="light-content" />
 
