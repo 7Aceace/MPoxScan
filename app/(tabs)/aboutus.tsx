6a72d02e790f0811fe9889e7d01c79fbe8e9eca9
Ace Monte <ace.monte27@gmail.com>
2024-11-13 13:17:41 +0800

    ResultPage prototype

--- app/(tabs)/aboutus.tsx
+++ app/(tabs)/aboutus.tsx
@@ -1,27 +1,43 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Pressable } from 'react-native';
 
 export default function AboutUs() {
   return (
   <SafeAreaView className="flex-1 items-center justify-center bg-green-400">
-      <View className="flex-1 flex-row items-start">
-        <View className='items-start'>
+      <View className="flex-row items-start justify-between px-5 pt-8 absolute top-0 w-full">
+        <View>
           <Text className="text-white text-xl">Back</Text>
         </View>
-        <View className='flex-1 items-center'>
-          <Text className="text-white text-5xl font-bold">Result</Text>
-        </View>
+      </View>
+      <View className="absolute top-16 w-full">
+        <Text className="text-white text-5xl font-bold text-center">Result</Text>
       </View>
 
-      <View className='flex-1'>
-
+      <View className="absolute top-40 w-full items-center justify-center">
+        <View className="w-60 h-60 bg-gray-300 rounded-full mt-4"></View>
+        <Text className="text-white text-3xl font-bold mt-8">Monkeypox</Text>
       </View>
 
-      <View className='flex-1 flexDirection-row items-center padding-5 justify-center bg-white  rounded-tl-3xl rounded-tr-3xl'>
-        <Text>
-            BLA BLA BLA BLA
+      <ScrollView 
+        className="absolute bottom-0 left-0 right-0 bg-white p-5 rounded-t-3xl h-[50%] pt-10"
+        style={{ flex: 1 }} 
+        showsVerticalScrollIndicator={true}
+        indicatorStyle="black"
+      >
+        <Text className="text-black text-2xl font-bold">
+          Key Information
         </Text>
+        <Text className="text-black">"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        </Text>
+      </ScrollView>
+      <View className="absolute bottom-10 left-0 right-0 items-center">
+        <Pressable className="bg-green-500 rounded-full py-3 px-6" onPress={() => console.log('Button Pressed')}>
+          <Text className="text-white text-lg font-bold">Standard Operating Procedure</Text>
+        </Pressable>
       </View>
     </SafeAreaView>
   );
