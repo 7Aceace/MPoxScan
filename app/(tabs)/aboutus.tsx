de50540448eba3e2c731c4dceb8f5adde7203eed
Ace Monte <ace.monte27@gmail.com>
2024-11-21 08:01:23 +0800

    Improved SOP and initial formatting for firebase

--- app/(tabs)/aboutus.tsx
+++ app/(tabs)/aboutus.tsx
@@ -18,7 +18,7 @@ export default function AboutUs() {
       <View className="absolute top-16 w-full">
         <Text className="text-white text-5xl font-bold text-center">Result</Text>
       </View>
-    
+      {/*
       <View className="flex-1 w-full px-4 mt-32 mb-4">
       {imageBase64 && (
           <View className="mb-4 items-center">
@@ -32,6 +32,26 @@ export default function AboutUs() {
           </View>
         )}
       </View>
+    */}
+
+
+      <View className="absolute top-40 w-full items-center justify-center">
+      {imageBase64 && (
+          <View className="mb-4 items-center">
+            <View className="w-64 h-64 rounded-full overflow-hidden border-4 border-white bg-white">
+              <Image
+                source={{ uri: `data:image/png;base64,${imageBase64}` }}
+                className="w-full h-full"
+                resizeMode="cover"
+              />
+            </View>
+          </View>
+        )}
+        {prediction && (
+            <Text className="text-white text-2xl font-bold flex-1 center mb-4">{prediction}</Text>
+          
+        )}
+      </View>
 
       <ScrollView 
         className="absolute bottom-0 left-0 right-0 bg-white p-5 rounded-t-3xl h-[45%] pt-10"
@@ -39,11 +59,10 @@ export default function AboutUs() {
         showsVerticalScrollIndicator={true}
         indicatorStyle="black"
       >
-        <Text className="text-lg font-bold mb-2">Prediction:</Text>
-           {prediction && (
-            <Text className="text-black text-2xl font-bold flex-1 center mb-4">{prediction}</Text>
-          
-        )}
+        
+        <Text className="text-black text-2xl font-bold">
+            Key Information
+        </Text>
         <Text className="text-black">"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
         "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
         "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
